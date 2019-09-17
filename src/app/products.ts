e70f06bbe77e76c3d46c72abeeb90399ac967a37
Ubuntu <ubuntu@ip-172-31-26-206.ec2.internal>
2019-09-17 08:12:07 +0000

    Angular Store App Commit

---
+++ src/app/products.ts
@@ -0,0 +1,19 @@
+export const products = [
+  {
+    name: 'IPhone X',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'IPhone XL',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'IPhone 8',
+    price: 299,
+    description: ''
+  }
+];
+
+
