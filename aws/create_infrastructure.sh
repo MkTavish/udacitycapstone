9ed63027c6bab801c5ca4010fbab5161d66b9250
EC2 Default User <ec2-user@ip-172-31-84-117.ec2.internal>
2020-05-17 18:14:29 +0000

    added comment to create_infrastructure.sh

--- aws/create_infrastructure.sh
+++ aws/create_infrastructure.sh
@@ -1,1 +1,2 @@
+//create infrastructure stack
 aws cloudformation create-stack --stack-name udacity-capstone-infra --template-body file://infrastructure.yml --parameters file://infrastructure_parameters.json
